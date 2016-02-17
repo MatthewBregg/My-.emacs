e86bca73fbed0ba4ef94cbbbe8b1cfab0c700d0e
matt <matthewbregg@gmail.com>
2016-02-17 11:55:24 -0500

    Fixed autoload for auto-resize

--- .emacs.d/elpa/auto-resize-1.0/auto-resize-autoloads.el
+++ .emacs.d/elpa/auto-resize-1.0/auto-resize-autoloads.el
@@ -2,14 +2,6 @@
 ;;
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../../../../../home/ultimanium/.emacs.d/elpa/auto-resize-1.0/auto-resize-autoloads.el"
-;;;;;;  "../../../../../../../../home/ultimanium/.emacs.d/elpa/auto-resize-1.0/auto-resize-pkg.el"
-;;;;;;  "../../../../../../../../home/ultimanium/.emacs.d/elpa/auto-resize-1.0/auto-resize.el"
-;;;;;;  "auto-resize-pkg.el") (21918 65392 786885 422000))
-
-;;;***
-
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
